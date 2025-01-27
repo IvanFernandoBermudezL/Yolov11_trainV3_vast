a72207a8680a64399b8e0323c0643d1dac6cee82
ifbermudez@uniquindio.edu.co <ifbermudez@uniquindio.edu.co>
2025-01-27 18:45:16 -0500

    segundo commit

--- yolo11.py
+++ yolo11.py
@@ -6,14 +6,11 @@ model = YOLO("yolo11n.pt")
 def main():
     # Train 
     model.train(
-    data="/Users/ivanbermudez/Downloads/Yolov11_prueba_externa/Ditch_Yolov11/data.yaml", 
-    epochs=10, 
+    data="", 
+    epochs=150, 
     batch=8, 
     imgsz=640, 
-
-    device="mps",
     save_period=5)
 
 if __name__ == '__main__':
     main()
-
